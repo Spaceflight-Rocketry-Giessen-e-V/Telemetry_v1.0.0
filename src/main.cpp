7726e646491119a23c71b24e6fe114815006f875
SpyAgentJo3 <156714735+SpyAgentJo3@users.noreply.github.com>
2024-04-07 22:24:23 +0200

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,58 @@
 #include <Arduino.h>
+#include <stdio.h>
 
-// put function declarations here:
-int myFunction(int, int);
+//Pin allocation
+int ledpin1 = PIN_PD3;
+int ledpin2 = PIN_PD4;
+int modepin = PIN_PD5;
+int cfgpin = PIN_PD6;
+int rstpin = PIN_PD7;
+
+
+//UART0 = Radiomodul
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+ //Pin initialisation
+  {
+    pinMode(ledpin1, OUTPUT);
+    pinMode(ledpin2, OUTPUT);
+    pinMode(modepin, OUTPUT);
+    pinMode(cfgpin, OUTPUT);
+    pinMode(rstpin, OUTPUT);
+  }
+    //Pin standard mode
+  {
+    digitalWrite(modepin, HIGH);
+    digitalWrite(cfgpin, HIGH);
+    digitalWrite(rstpin, HIGH);
+    
+    digitalWrite(ledpin1, LOW);
+    digitalWrite(ledpin2, LOW);
+
+  }
+  //Serial configuration
+  {
+    Serial.begin(19200);
+    Serial0.begin(19200);
+  }
+  
+  //Waiting for first user input
+  {
+    while(Serial.available() == 0);
+    Serial.read();
+    Serial.print("Setup complete. Entering normal operation mode. | ");
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  //Test programm which indicates a fully functioning UPDI-programmer
+  for(int i=0; i<5; i++)
+  {
+    digitalWrite(ledpin1, HIGH);
+    delay(500);
+    digitalWrite(ledpin2, HIGH);
+    delay(1000);
+    digitalWrite(ledpin1, LOW);
+    digitalWrite(ledpin2, LOW);
+  }
 }
