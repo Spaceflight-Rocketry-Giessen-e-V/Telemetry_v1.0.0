1e3ddf3a2c49b492464dd8c5f1fd848d1cf30a22
SpyAgentJo3 <156714735+SpyAgentJo3@users.noreply.github.com>
2024-05-31 23:39:32 +0200

    Basic string copy and radio transmission

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,41 @@
-#include <Arduino.h>
-#include <stdio.h>
+#define cfgpin PIN_PD6
+#define rstpin PIN_PD7
+#define modepin PIN_PD5
 
-//Pin allocation
-int ledpin1 = PIN_PD3;
-int ledpin2 = PIN_PD4;
-int modepin = PIN_PD5;
-int cfgpin = PIN_PD6;
-int rstpin = PIN_PD7;
+#include <avr/io.h>
 
-
-//UART0 = Radiomodul
+int inByte;
+int outByte;
+char inBuffer[8]={0};
+char OutBuffer[4]={0};
 
 void setup() {
- //Pin initialisation
-  {
-    pinMode(ledpin1, OUTPUT);
-    pinMode(ledpin2, OUTPUT);
-    pinMode(modepin, OUTPUT);
-    pinMode(cfgpin, OUTPUT);
-    pinMode(rstpin, OUTPUT);
-  }
-    //Pin standard mode
-  {
-    digitalWrite(modepin, HIGH);
-    digitalWrite(cfgpin, HIGH);
-    digitalWrite(rstpin, HIGH);
-    
-    digitalWrite(ledpin1, LOW);
-    digitalWrite(ledpin2, LOW);
+  //set PD5 (mode), PD6 (cfg), PD7 (rst) as output
+  PORTD.DIRSET = PIN5_bm | PIN6_bm | PIN7_bm;  
 
-  }
-  //Serial configuration
-  {
-    Serial.begin(19200);
-    Serial0.begin(19200);
-  }
-  
-  //Waiting for first user input
-  {
-    while(Serial.available() == 0);
-    Serial.read();
-    Serial.print("Setup complete. Entering normal operation mode. | ");
-  }
+  //Pin standard mode
+  PORTD.OUTSET = PIN7_bm | PIN6_bm; //set PD7 and PD6 to HIGH
+
+  Serial2.begin(19200); //referring to serial monitor
+  Serial0.begin(19200); //referring to radio module
 }
 
 void loop() {
-  //Test programm which indicates a fully functioning UPDI-programmer
-  for(int i=0; i<5; i++)
-  {
-    digitalWrite(ledpin1, HIGH);
-    delay(500);
-    digitalWrite(ledpin2, HIGH);
-    delay(1000);
-    digitalWrite(ledpin1, LOW);
-    digitalWrite(ledpin2, LOW);
+  //do nothing until something is available on RX0
+  while(Serial0.available() == 0)
+    Serial2.write("Nothing available");
+
+  //copy received String into inBuffer
+  for(int i=0; Serial0.available() != 0; i++) {
+    inBuffer[i]=Serial0.read();
+    Serial2.write("Succesfully copied Byte %i", i);
+  }  
+  //transmit received string back to ground station
+  for(int i=0; i<4; i++) {
+    Serial0.write(inBuffer[i]);
+    Serial2.write("Succesfully transmitted byte %i",i);
   }
+
+  Serial2.write("String transmitted");
+
 }
